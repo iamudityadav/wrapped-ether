83b8d61276d7a78d6c4ce2c4c4e237619380d426
Udit Yadav <askudityadav@gmail.com>
2023-11-25 23:53:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Wrapped Ether(WETH)
+
+This project demonstrates the Wrapped Ether smart contract.
+
+Try running some of the following tasks:
+
+```shell
+npm install
+npx hardhat compile
+```
---
+++ contracts/WETH.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WETH is ERC20 {
+    event Deposit(address indexed account, uint256 indexed amount);
+    event Withdraw(address indexed account, uint256 indexed amount);
+
+    constructor() ERC20("Wrapped Ether", "WETH") {}
+
+    fallback() external payable {
+        deposit();
+    }
+
+    receive() external payable {
+        deposit();
+    }
+
+    function deposit() public payable {
+        _mint(msg.sender, msg.value);
+
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 _amount) external {
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(_amount);
+
+        emit Withdraw(msg.sender, _amount);
+    }
+}
